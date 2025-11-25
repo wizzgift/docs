2ba321cf89957da88bb0e2878a7feaa08cd6d599
Anil Kunwar <107930100+anilkunwar421@users.noreply.github.com>
2025-11-25 12:01:56 +0545

    Documentation edits made through Mintlify web editor

---
+++ custom.js
@@ -0,0 +1,15 @@
+<script>
+  (function(d,t) {
+    var BASE_URL="https://desk.sapniverse.com";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: 'LsqZ3F96E3eF7LJzQgDpUH6R',
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script");
+</script>
